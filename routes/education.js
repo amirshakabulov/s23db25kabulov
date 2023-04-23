ab3a3631bcf557c222ffe414334dbf55278da027
amirshakabulov <kabulovamirsha@gmail.com>
2023-04-23 17:37:12 -0500

    added registration/login&authentication

--- routes/education.js
+++ routes/education.js
@@ -1,22 +1,30 @@
 var express = require('express');
 const education_controllers= require('../controllers/education');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+   if (req.user) {
+     return next();
+   }
+   req.session.returnTo = req.originalUrl;
+   res.redirect("/login");
+ };
 
 /* GET education */
 router.get('/', education_controllers.education_view_all_Page );
 /* GET detail education page */
 router.get('/detail', education_controllers.education_view_one_Page)
 //Create education page
-router.get('/create', education_controllers.education_create_Page)
-/* GET create update education page */
-router.get('/update', education_controllers.education_update_Page);
+router.get('/create', secured, education_controllers.education_create_Page)
+/* GET update education page */
+router.get('/update', secured, education_controllers.education_update_Page);
 /* GET delete education page */
-router.get('/delete', education_controllers.education_delete_Page);
+router.get('/delete', secured, education_controllers.education_delete_Page);
 
-
-
-router.get('/', function(req, res) {
-	res.render('education', { title: 'Search Result for Education'});
+router.get('/register', function(req, res) {
+	res.render('register', { title: 'Education App Registration'});
    });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,63 @@
-var express = require('express');
+var express = require("express");
+var passport = require("passport");
 var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Amirsha Kabulov', results: this.results });
+var Account = require("../models/account");
+router.get("/", function (req, res) {
+  res.render("index", { title: "Education App", user: req.user });
 });
-
+router.get("/register", function (req, res) {
+  res.render("register", { title: "Education App Registration" });
+});
+// it gave me an error of findOne() no longer accepts a callback, so modified given code a bit to make it work. Logic stays the same.
+router.post("/register", function (req, res) {
+  Account.findOne({ username: req.body.username })
+  .then(user => {
+    if (user) {
+      return res.render("register", {
+        title: "Registration",
+        message: "Existing User",
+        account: req.body.username,
+      });
+    }
+    let newAccount = new Account({ username: req.body.username });
+    Account.register(newAccount, req.body.password, function (err, user) {
+      if (err) {
+        return res.render("register", {
+          title: "Registration",
+          message: "access error",
+          account: req.body.username,
+        });
+      }
+      console.log("Sucess, redirect");
+      res.redirect("/");
+    });
+  })
+  .catch(err => {
+    return res.render("register", {
+      title: "Registration",
+      message: "Registration error",
+      account: req.body.username,
+    });
+  });
+});
+router.get("/login", function (req, res) {
+  res.render("login", { title: "Education App Login", user: req.user });
+});
+router.post("/login", passport.authenticate("local"), function (req, res) {
+	if(req.session.returnTo){
+ 		res.redirect(req.session.returnTo);
+	}
+	res.redirect("/");
+});
+router.get("/logout", function (req, res) {
+  req.logout( function(err) {
+	if (err) {
+		return next(err);
+	}
+	res.redirect('/');
+  });
+});
+router.get("/ping", function (req, res) {
+  res.status(200).send("pong!");
+});
 module.exports = router;
